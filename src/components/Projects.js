4cc5e30a931157184e519802b58d2ca55d85140d
layken varholdt <laykenv@gmail.com>
2023-07-27 12:33:36 -0500

    added carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,19 @@
 import React from "react";
 import logo from "./chili.jpeg";
-import pbaPic1 from "../images/IWAI_screenshot1.png";
-import pbaPic2 from "../images/IWAI_screenshot2.png";
-import fmPic1 from "../images/PBA_pic1.png";
-import fmPic2 from "../images/PBA_pic2.png";
+import IWAIPic1 from "../images/IWAI_screenshot1.png";
+import IWAIPic2 from "../images/IWAI_screenshot2.png";
+import pbaPic1 from "../images/PBA_pic1.png";
+import pbaPic2 from "../images/PBA_pic2.png";
 import sparq1 from "../images/sparq 1.png";
 import sparq2 from "../images/sparq 2.png";
+import Carousel from "react-material-ui-carousel";
 import "./projects.css";
+
+const projectPhoto = (photo) => {
+    <div className="projectPhotoDiv">
+        <img className="projectPhoto" src={photo} alt="Application Screenshot"></img>
+    </div>
+}
 
 
 function Projects (props) {
@@ -28,12 +35,12 @@ function Projects (props) {
                         <button className="projectLinkButton" onClick={() => { openTab("https://interview-with-ai.vercel.app/") }}>See it Live</button>
                     </div>
                 </div>
-                <img className="projectPhoto" src={pbaPic2} alt="ferrari"></img>
-                <img className="projectPhoto" src={pbaPic1} alt="ferrari"></img>
+                <Carousel className="carousel" animation="slide" interval={5000}>
+                    <img className="projectPhoto" src={IWAIPic1} alt="Application Screenshot"></img>                    
+                    <img className="projectPhoto" src={IWAIPic2} alt="Application Screenshot"></img>                    
+                </Carousel>
             </div>
             <div className="pokemonDiv">
-                <img className="projectPhoto" src={sparq1} alt="ferrari"></img>
-                <img className="projectPhoto" src={sparq2} alt="ferrari"></img>
                 <div className="projectInfo">
                     <div className="projectHeader">Sparq Scheduling App</div>
                     <div className="projectDescription">A scheduling app I helped create while part of an internship at Sparq. I utilized scrum and agile methodologies while working on a team of 4 developers, a Product Owner, a Scrum Master, a QA tester, as well as a board of stakeholders that we demo'd weekly to in a presentation. This app is built with in React and utilizes every library I have mentioned in the graphic above.</div>
@@ -42,8 +49,12 @@ function Projects (props) {
                         <button className="projectLinkButton" onClick={() => { openTab("https://laykenv.github.io/sparqSchedulingApp") }}>See it Live</button>
                     </div>
                 </div>
+                <Carousel className="carousel" animation="slide" interval={5000}>
+                    <img className="projectPhoto" src={sparq1} alt="Application Screenshot"></img>                    
+                    <img className="projectPhoto" src={sparq2} alt="Application Screenshot"></img>                    
+                </Carousel>
             </div>
-            <div className="ferrariDiv">
+            <div className="pokemonDiv">
                 <div className="projectInfo">
                     <div className="projectHeader">Pokemon Battle App</div>
                     <div className="projectDescription">A Pokemon battle simulator that allows the user to choose a team of Pokemon and battle against their rival. Built with Typescript, React, and unit tested with Jest. All Pokemon data pulled from PokeAPI.</div>
@@ -52,8 +63,10 @@ function Projects (props) {
                         <button className="projectLinkButton" onClick={() => { openTab("https://laykenv.github.io/pokemon-battle-app/") }}>See it Live</button>
                     </div>
                 </div>
-                <img className="projectPhoto" src={fmPic1} alt="ferrari"></img>
-                <img className="projectPhoto" src={fmPic2} alt="ferrari"></img>
+                <Carousel className="carousel" animation="slide" interval={5000}>
+                    <img className="projectPhoto" src={pbaPic1} alt="Application Screenshot"></img>                    
+                    <img className="projectPhoto" src={pbaPic2} alt="Application Screenshot"></img>                    
+                </Carousel>
             </div>
         </div>
     )
